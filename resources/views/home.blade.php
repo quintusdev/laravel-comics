f875063d328e092f8ca72067c89f96bfde4803c1
Quintusdev <quintusdev92@gmail.com>
2023-07-21 15:20:41 +0200

    [STYLE] INSERIMENTO STILE NAVBAR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- LINK FONTAWESOME -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
         <title>Laravel</title>
 
         <!-- Styles -->
@@ -16,15 +19,15 @@
         <header>
             <div class="container">
                 <div class="row">
-                    <div class="col-12 d-flex justify-content-center align-items-center mt-4">
+                    <div class="col-10 d-flex justify-content-center align-items-center mt-4">
                         <img class="m-2" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
                         <div class="navbar-nav container navbar-light">
-                            <ul class="list-unstyled d-flex justify-content-center text-uppercase mb-0">
+                            <ul class="list-unstyled d-flex justify-content-center align-items-center text-uppercase">
                                 <li class="nav-item px-2 py-2">
                                     <a class="nav-link">CHARACTERS</a>
                                 </li>
-                                <li class="nav-item px-2 py-2">
-                                    <a class="nav-link {{ Route::currentRouteName() === 'homepage' ? 'fw-bold active' : ''}}" href="{{ route('homepage') }}">COMICS</a>
+                                <li class="nav-item px-2 py-2 {{ Route::currentRouteName() === 'homepage' ? 'fw-bold active_li' : ''}}">
+                                    <a class="nav-link {{ Route::currentRouteName() === 'homepage' ? 'fw-bold active_a' : ''}}" href="{{ route('homepage') }}">COMICS</a>
                                 </li>
                                 <li class="nav-item px-2 py-2">
                                     <a class="nav-link">MOVIES</a>
@@ -48,18 +51,27 @@
                                     <a class="nav-link">NEWS</a>
                                 </li>
                                 <li class="nav-item px-2 py-2">
-                                    <a class="nav-link">SHOP</a>
+                                    <a class="nav-link d-flex align-items-center">SHOP <i class="fa-solid fa-caret-down" style="color: #0074e8;"></i></a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div class="col-12">
-
+                    <div class="col-2 d-flex justify-content-center align-items-center mt-4">
+                        <div class="search d-flex s-control">
+                            <input type="text" class="form-control s-bar" placeholder="Search...">
+                            <i class="fa fa-search align-self-center"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </header>
-        
+        <main>
+            <div class="row">
+                <div class="col-12">
+                    <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+                </div>
+            </div>
+        </main>
 
     </body>
 
