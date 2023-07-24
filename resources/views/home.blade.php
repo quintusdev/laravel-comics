6a2ebaceb89f8f9e9e84edd68e7ec3b7e39ca7b3
Quintusdev <quintusdev92@gmail.com>
2023-07-24 13:06:18 +0200

    [FEATURES] Creazione file cimc, app.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- LINK FONTAWESOME -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
-        <title>Laravel</title>
-
-        <!-- Styles -->
-        @vite('resources/js/app.js')
-
-    </head>
-
-    <body>
-        @include('partials.header')
-        <main>
             <!-- JUMBOTRON -->
             <img class="jumbotron" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
             <!-- CARD COMICS -->
@@ -121,6 +102,3 @@
                     </div>
                 </div>
             </div>
-        </main>
-    </body>
-</html>
