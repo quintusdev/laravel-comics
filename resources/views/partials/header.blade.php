bb460e26d24bf4e799238c1ea02c38c5c5f31ebc
Quintusdev <quintusdev92@gmail.com>
2023-07-24 09:55:06 +0200

    [FEATURES] inserita barra header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,11 @@
 <header>
+    <div class="credit d-flex justify-content-end align-items-center px-5">
+        <span class="px-5">DC POWER&trade;VISA&reg;</span>
+        <span>ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></span> 
+    </div>
     <div class="container">
         <div class="row">
-            <div class="col-10 d-flex justify-content-center align-items-center mt-2">
+            <div class="col-10 d-flex justify-content-center align-items-center">
                 <img class="m-2" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
                 <div class="navbar-nav container navbar-light">
                     <ul class="list-unstyled d-flex justify-content-center align-items-center text-uppercase">
@@ -38,7 +42,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-2 d-flex justify-content-center align-items-center mt-2">
+            <div class="col-2 d-flex justify-content-center align-items-center">
                 <div class="search d-flex s-control">
                     <input type="text" class="form-control s-bar" placeholder="Search...">
                     <i class="fa fa-search align-self-center"></i>
