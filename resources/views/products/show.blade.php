82234c9b2ee81ad91e3dbf5b7e5fb8a3fc2d28dd
Quintusdev <quintusdev92@gmail.com>
2023-07-24 14:54:09 +0200

    [STYLE]ES-COMPLETATO - da sistemare img pubblicità

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,8 +8,8 @@
                     <div class="col-8">
                         <img class="w-50" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                         <div class="p-3 mt-4">
-                            <h4><strong>{{ $comic['title'] }}</strong></h4>
-                            <p><strong>Prezzo:  </strong>{{ $comic['price'] }}</p>
+                            <h4 class="text-primary"><strong>{{ $comic['title'] }}</strong></h4>
+                            <p class="text-success"><strong>Prezzo:  </strong>{{ $comic['price'] }}</p>
                             <p><strong>Serie:  </strong>{{ $comic['series'] }}</p>
                             <p><strong>Tipo:  </strong>{{ $comic['type'] }}</p>
                             <p><strong>Data uscita:  </strong>{{ $comic['sale_date'] }}</p>
@@ -17,6 +17,7 @@
                         </div>
                     </div>
                     <div class="col-4">
+                        <h6 class="text-secondary"><strong>ADVERTISMENT</strong></h6>
                         <img class="w-100" src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="Immagine Pubblicità">
                     </div>
                 </div>
